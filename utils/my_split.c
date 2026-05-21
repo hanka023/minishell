1f0d04de6c6912071f692d506698c753662252d2
hanka023 <hankaskalova02@gmail.com>
2026-05-21 14:14:02 +0200

    buggy

--- utils/my_split.c
+++ utils/my_split.c
@@ -2,6 +2,27 @@
 #include "../minishell.h"
 
 
+size_t	ft_strlcpy(char *dest, const char *src, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	if (n > 0)
+	{
+		while ((i < (n - 1)) && (src[i] != '\0'))
+		{
+			dest[i] = src[i];
+			i++;
+		}
+		dest[i] = '\0';
+	}
+	i = 0;
+	while (src[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
 
 char	*my_copy_word(char *start, char *end)
 {
@@ -44,7 +65,7 @@ char	*my_copy_word(char *start, char *end)
 t_list *my_split(char  *str)
 {
 	char	*copy;
-	int		i;
+	//int		i;
 	//int j;
 	t_list *head;
 	t_list *lst;
@@ -53,71 +74,64 @@ t_list *my_split(char  *str)
 	// char z1 = '\'';
 	// char z2 = '\"';
 	// char pipe = '|';
-	// char c = ' ';
+	//	char c = ' ';
 	// char c1 = '<';
 	// char c2 = '>';
 
 	//char *set = " \t\n";
 	head = NULL;
-	copy = str;
-	i = 0;
+
+	//i = 0;
 	int len = 0;
 	//j = 0;
 	
-	// char *start;
-	// char *end;
+	char *start;
+	int token = 0;
+	ssize_t scpy = 0;
 
-	// start = str;
-	
-
-	while (str[i] != '\0')
+	while (*str != '\0')
 	{
-		if (str[i] == '\"')
+		
+		while (*str == ' ' && *str !='\0')
+			++str;
+		if (*str != ' ')
 		{
-			++i;
-			while(str[i] != '\'' && str[i] !='\0')
-				++i;
-			if  (str[i] == '\"')
-				++i;
-			
-		}	
-		else if (str[i] == '\'')
-		{
-			++i;
-			while(str[i] != '\'' && str[i] !='\0')
-				++i;
-			if  (str[i] == '\'')
-				++i;
-			
-		}	
-		else if (str[i] != ' ')
-		{
-			while (str[i] !=' ')
-				++i;
+			start = str;
+			while(*str != ' ' && *str !='\0' )
+				++str;
+				
 		}
-		if (str[i] == ' ')
+		if (*str == ' ')
 		{
-			len = i;
-			i = 0;
-			while (i < len)
-			{
-				copy[i] = str[i];
-				++i;
-			}
-			copy[i] = '\0';
-			lst = my_str_to_lst(&head, copy);
-			printf("token %s\n", lst-> str);
+			len = str - start;
+			copy = malloc (sizeof (char) * (len + 1));
+			if (copy == NULL)
+   				return (NULL);
+			scpy = ft_strlcpy (copy, start, len + 1);
+			if (!scpy)
+				return (0);
+			
+		
+			lst = new_list(copy);
+			printf("list %s\n", lst -> str);
+		
 
+			add_back(&head, lst); 
+
+			free (copy);
+			//printf("token %d %s\n", token, lst-> str);
+			++token;
+			++str;
 		}
+		//++str;
 	}
-
-	char *copy_test = str;
+	//char *copy_test = str;
 	//printf ("testtttt %s\n", str);
 
 //	lst = my_str_to_lst(&head, copy);
 //	printf("list v my_split %s\n", lst-> str);
 
-	lst = my_str_to_lst(&head, copy_test);
+	//lst = my_str_to_lst(&head, copy_test);
 
 	//printf("list v my_split - copy_test %s\n", lst-> str);
 	return (head);
