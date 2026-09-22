677cfbb44803380a8ae52c69ebf1b601bafd54af
Hana Skalova <haskalov@c3r2s4.42prague.com>
2026-09-23 01:29:07 +0200

    mini

--- main.c
+++ main.c
@@ -3,49 +3,31 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: haskalov <haskalov@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jkralice <jkralice@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/26 16:53:10 by jkralice          #+#    #+#             */
-/*   Updated: 2026/09/22 21:30:39 by haskalov         ###   ########.fr       */
+/*   Updated: 2026/09/22 21:54:24 by jkralice         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//	jkralice
+
 #include "mini.h"
 #include "jkralice/Lib42/lib42.h"
 
-//	stdlib
-#include <unistd.h>
-
-//	readline
-#include <readline/readline.h>
-#include <readline/history.h>
-
-
-int	main(void)
+int	main(int argc, char **argv, char **envp)
 {
 	t_state	state;
-	char	*line;
+	t_list	*tokens;
 
-	state = setup(gib(1), 2, mib(128));
-	while (1)
+	(void)argc;
+	(void)argv;
+	state = setup(envp, gib(1), 2, mib(128));
+	state.running = 1;
+	while (state.running)
 	{
-		line = readline(PROMPT);
-        if (!line) // NULL = EOF / Ctrl+D
-            break ;
-		if (line[0] == '\0')
-        {
-            free(line);
-            continue ;
-        }
-		add_history(line);
-
-		state.list = input(&state, line);
-		interpret(&state);
-		//print_list(state.list);
-		free_list(state.list);
+		tokens = input(&state);
+		interpret(&state, tokens);
 	}
-	rl_clear_history();
 	cleanup(state);
 	return (0);
 }
--- mini.h
+++ mini.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   mini.h                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: haskalov <haskalov@student.42.fr>          +#+  +:+       +#+        */
+/*   By: jkralice <jkralice@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/26 16:53:43 by jkralice          #+#    #+#             */
-/*   Updated: 2026/09/21 20:58:32 by haskalov         ###   ########.fr       */
+/*   Updated: 2026/09/22 23:04:19 by jkralice         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,25 +20,35 @@
 #  define PROMPT "\e[032m[ mini ] > \e[0m"
 # endif
 
+# ifndef ERR_MSG
+#  define ERR_MSG "\e[031m[!]\e[0m mini error: "
+# endif
+
+//	Global state of minishell.
 typedef struct s_state
 {
-	char		**envp;
-	t_arena		*arena;
-	t_list		*list;
-	t_ppl		*ppl;
-	int			exit_code;
+	t_arena		*arena;		//	Global arena.
+	char		**envp;		//	Environment variables.
+	t_ppl		*ppl;		//	Shell's pipeline. (cached for memory reuse)
+	int			exit_code;	//	Last processe's exit code.
+	int			running;	//	Self explanatory.
 }	t_state;
 
-//	Sets up the minishell's state.
-t_state	setup(size_t arena_size, size_t scratch_count, size_t scratch_size);
+//	Sets up minishell's state.
+t_state	setup(
+			char **envp,
+			size_t arena_size,
+			size_t scratch_count,
+			size_t scratch_size
+			);
+
+//	Gather and parse input.
+t_list	*input(t_state *state);
+
+//	Interprets and runs the input.
+void	interpret(t_state *state, t_list *tokens);
 
 //	Cleans up state before exit.
 void	cleanup(t_state state);
-
-//	Gather and parse input.
-t_list	*input(t_state *state, char *line);
-
-//	Interprets and runs the input.
-void	interpret(t_state *state);
 
 #endif
